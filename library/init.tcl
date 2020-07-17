251294080e014ca1431991e642f224bccc800896
Peter da Silva <peter.dasilva@flightaware.com>
2020-07-17 19:43:48 +0000

    Add tcl2020 configuration

--- library/init.tcl
+++ library/init.tcl
@@ -817,3 +817,11 @@ proc tcl::CopyDirectory {action src dest} {
     }
     return
 }
+
+# tcl2020 configuration
+namespace eval ::tcl2020 {
+    variable info
+    set info(version) "1.0.0"
+    variable features
+    set features(named parameters) "1.0.0"
+}
